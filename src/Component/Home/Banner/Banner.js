e4b483ce0beeda078aa8714ff23f17c1abee276f
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-09-23 10:11:04 +0600

    first commit

---
+++ src/Component/Home/Banner/Banner.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../Header/Header';
+import './Banner.css'
+
+const Banner = () => {
+    // const [data, setData] = useState([])
+    // const [dataCount, setDataCount] = useState({})
+    // let [count, setCount] = useState(0)
+
+    // useEffect(() => {
+    //     fetch('banner.json')
+    //     .then(res => res.json())
+    //     .then(data => setData(data))
+    // }, [])
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         ++count
+    //         console.log(count)
+    //         if(count >= data.length){
+    //             count = 0
+    //         }
+    //         setCount(count)
+    //         setDataCount(data[count])
+    //     }, 5000)
+    // }, [])
+    
+    const a = 'https://medical-clinic.cmsmasters.net/wp-content/uploads/2016/09/bg-1.jpg'
+
+    return (
+        <section style={{backgroundImage: `url(${a})`}}>
+            <div>
+                <Header></Header>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;

69eaf5fbda820b7a402ca5476ddd0c85819e3db4
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-09-26 12:32:38 +0600

    header style

--- src/Component/Home/Banner/Banner.js
+++ src/Component/Home/Banner/Banner.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import Header from '../../Shared/Header';
 import './Banner.css'
 import bg1 from '../../../photos/bg-1.jpg'
@@ -8,14 +7,39 @@ import bg3 from '../../../photos/bg-3-1.jpg'
 import bg4 from '../../../photos/bg-4.jpg'
 
 const Banner = () => {
-    const data = [bg1, bg2, bg3, bg4]
-    const [dataCount, setDataCount] = useState(bg1)
+    const data = [
+        {
+            _id: "1",
+            firstTitle: "Medical services that",
+            secondTitle: "you can trust",
+            img: bg1
+        },
+        {
+            _id: "2",
+            firstTitle: "The Hospital of the ",
+            secondTitle: "Future, Today",
+            img: bg2
+        },
+        {
+            _id: "3",
+            firstTitle: "Medical excellence",
+            secondTitle: "every day.",
+            img: bg3
+        },
+        {
+            _id: "4",
+            firstTitle: "Exceptional people.",
+            secondTitle: "Exceptional care.",
+            img: bg4
+        },
+    ]
+    const [dataCount, setDataCount] = useState(data[0])
     let [count, setCount] = useState(0)
 
     useEffect(() => {
         const slider = setInterval(() => {
             ++count
-            if(count >= data.length){
+            if (count >= data.length) {
                 count = 0
             }
             setDataCount(data[count])
@@ -25,7 +49,7 @@ const Banner = () => {
     }, [])
 
     return (
-        <section className='banner' style={{backgroundImage: `url(${dataCount})`}}>
+        <section className='banner' style={{ backgroundImage: `url(${dataCount?.img})` }}>
             <div>
                 <Header></Header>
             </div>
--- src/Component/Shared/Header.js
+++ src/Component/Shared/Header.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../photos/logo.png'
 import './Header.css'
 
-const Header = ({location}) => {
+const Header = ({ location }) => {
+    const [scrollY, setScrollY] = useState(false)
+
+    function scrollFn(){
+        if(window.scrollY >= 200){
+            setScrollY(true)
+        }else{
+            setScrollY(false)
+        }
+    }
+    window.addEventListener('scroll', scrollFn)
+
     return (
-        <header className={`${location?.pathname === '/departments' && 'change-header-bg'}`}>
-            <nav className='small-nav'>
-                <ul>
-                    <li><Link to='' alt=''>FAQ</Link></li>
-                    <li><Link to='' alt=''>Book Appointment Contacts </Link></li>
-                    <li><Link to='' alt=''>example@example.com</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to='' alt=''><i className="fa-brands fa-facebook-f"></i></Link></li>
-                    <li><Link to='' alt=''><i className="fa-brands fa-instagram"></i></Link></li>
-                    <li><Link to='' alt=''><i className="fa-brands fa-twitter"></i></Link></li>
-                    <li><Link to='' alt=''><i className="fa-brands fa-youtube"></i></Link></li>
-                </ul>
-            </nav>
-            <nav className='main-nav'>
-                <Link to='' alt=''>
-                    <img src={logo} alt=''></img>
-                </Link>
-                <ul>
-                    <li><Link to='/' alt=''>Home</Link></li>
-                    <li><Link to='/departments' alt=''>Department</Link></li>
-                    <li><Link to='' alt=''>Timetable</Link></li>
-                    <li><Link to='' alt=''>Features</Link></li>
-                </ul>
-            </nav>
+        <header className={`${location?.pathname === '/departments' && 'change-header-bg'} ${scrollY && 'header-change-bg'}`}>
+            <div>
+                <nav className='small-nav'>
+                    <ul>
+                        <li><Link to='' alt=''>FAQ</Link></li>
+                        <li><Link to='' alt=''>Book Appointment Contacts </Link></li>
+                        <li><Link to='' alt=''>example@example.com</Link></li>
+                    </ul>
+                    <ul>
+                        <li><Link to='' alt=''><i className="fa-brands fa-facebook-f"></i></Link></li>
+                        <li><Link to='' alt=''><i className="fa-brands fa-instagram"></i></Link></li>
+                        <li><Link to='' alt=''><i className="fa-brands fa-twitter"></i></Link></li>
+                        <li><Link to='' alt=''><i className="fa-brands fa-youtube"></i></Link></li>
+                    </ul>
+                </nav>
+                <nav className='main-nav'>
+                    <Link to='/' alt=''>
+                        <img src={logo} alt=''></img>
+                    </Link>
+                    <ul>
+                        <li><Link to='/' alt=''>Home</Link></li>
+                        <li><Link to='/departments' alt=''>Department</Link></li>
+                        <li><Link to='' alt=''>Timetable</Link></li>
+                        <li><Link to='' alt=''>Features</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
